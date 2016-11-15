6e7da2e9b38ab83443d3082ef56050b4bd301a74
Seth Bertlshofer <celestials2013@gmail.com>
2016-11-14 22:09:48 -0700

    added sudoku parser

--- src/main/java/sudoku/sudoku.java
+++ src/main/java/sudoku/sudoku.java
@@ -1,11 +1,25 @@
 package sudoku;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * Created by seth on 11/14/16.
  */
 public class sudoku {
+    public int[][] board;
 
+    public sudoku(String fin) {
 
-    public sudoku() {
+    }
+
+    public void sudokuParser(String fin) {
+        try {
+            for (String line : Files.readAllLines(Paths.get(fin))) {
+                // ...
+            }
+        } catch (Exception e) {
+            System.out.print(e);
+        }
     }
 }
