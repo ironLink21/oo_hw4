36284fdee3427aa5e51cf000c17296d7194e34d4
Seth Bertlshofer <celestials2013@gmail.com>
2016-11-21 10:34:35 -0700

    trying to get unit tests to work

--- src/test/java/sudoku/SolverMethod1Test_v2.groovy
+++ src/test/java/sudoku/SolverMethod1Test_v2.groovy
@@ -1,31 +1,14 @@
 package sudoku
 
-import static java.lang.Boolean.FALSE
-import static java.lang.Boolean.TRUE
-
 /**
  * Created by seth on 11/18/16.
  */
-class SolverMethod1Test extends GroovyTestCase {
+class SolverMethod1Test_v2 extends GroovyTestCase {
     void testFindEmptyCell() {
         String validChars = "1 2 3 4";
         String[][] board = [["4","2","-","1"],["-","-","-","2"],["3","-","2","-"],["-","4","-","3"]];
 
         Solver method1 = new SolverMethod1(board, validChars).findEmptyCell();
         assert method1.emptyCoordinates == [[0,2],[1,0],[1,1],[1,2],[2,1],[2,3],[3,0],[3,2]];
-
-//        assert board == [["4","2","3","1"],["1","3","4","2"],["3","1","2","4"],["2","4","1","3"]];
     }
-
-//    void testCheckRow() {
-//
-//    }
-//
-//    void testCheckCol() {
-//
-//    }
-//
-//    void testSetPossibleValues() {
-//
-//    }
 }
